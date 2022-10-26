471521aaaf095014fd7c844b646a5cda03c92ae6
khawla-bk <benkadidakhawla@gmail.com>
2022-10-26 22:45:14 +0100

    added accordion to FAQ page and its route to app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NotFound from "./Pages/404/404";
 //import Login from './Pages/Login/Login';
 import Boosting from "./Pages/Boosting/Boosting";
 import Homepage from "./Pages/Homepage/Homepage";
+import Faq from "./Pages/FAQ/FAQ";
 import "./App.scss";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" element={<Homepage />} />
         <Route path="/404" element={<NotFound />} />
         <Route path="/boosting" element={<Boosting />} />
+        <Route path="/faq" element={<Faq />} />
       </Routes>
     </div>
   );
---
+++ src/Pages/FAQ/FAQ.js
@@ -0,0 +1,18 @@
+import React from "react";
+import CustomAccordion from "../../Components/Accordion/Accordion";
+import Header from "../../Layout/Header/Header";
+import Footer from "../../Layout/Footer/Footer";
+
+function Faq() {
+  return (
+    <>
+      <Header />
+      <div>
+        <CustomAccordion />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Faq;
